8aa6d7048b7310f52f8aa82fd823c371fe0250c5
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-12-20 19:06:38 +0500

    Variables ==== var

---
+++ app.js
@@ -0,0 +1,26 @@
+//  Variables  ===== "var"
+
+/* variable ese use hote hn 
+   aur variables value k liye use hote hn
+   k koi bhi value varibles me rakh skte hn 
+*/
+var a = "hello";
+var b = "World";
+// document.write(a + b)
+
+//  Variables likhne ka sai tarika
+// Camel case or snack case
+
+var firstName = "Abdur Rehman ";
+var last_name = "Ahmed";
+// document.write(firstName + last_name)
+
+/*  variables ka ek bar keyword likhe k bht sare variable k naam de
+    k baad me variables k name se use kr sakte hn coma dal dal k.. 
+*/
+var first_name, last_name, sur_name;
+first_name = "abdur rehman ";
+last_name = "ahmed ";
+sur_name = "khan ";
+
+// document.write(first_name + last_name + sur_name)
