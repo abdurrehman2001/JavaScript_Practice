ee94e97a82fda63a1144d8fe8100132d7b4b0b25
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-12-21 08:46:03 +0500

    Let or Const Variables

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//  Variables  ===== "var"
+//                                   Variables  ===== "var"
 
 /* variable ese use hote hn 
    aur variables value k liye use hote hn
@@ -24,3 +24,26 @@ last_name = "ahmed ";
 sur_name = "khan ";
 
 // document.write(first_name + last_name + sur_name)
+
+//                                  Let or Const Variables
+
+/*  Let name ke variable me ye different hai k ye jab ek naam se declare ho jata hai
+    to dubara ussi name se let naam k variable ko declare nhi kr skte han value update
+    kr skte hen var ki trhn baki sab var wale variable ki trhn hi let kam krta hai...
+*/
+
+let first = "hello";
+first = "world";
+// let first = "papa";   // ye smjhane k liye ussi name se declare kiya hai...
+// document.write(first)
+
+//              Const Variable ====> const stant for constant variable
+
+/*  const variable jab ek bar declare ho jata hai to usko na ussi naam se declare kr skte hen
+    na hi uski value ko update kr skte hen
+*/
+
+const first_value = "hello world";
+//first_value = "hellooo"; // aur na hi value ko update kr skte hen..
+// const first_value ="Hello duniya" // ye smjhane k liye k ussi name se declare nhi kr skte 
+document.write(first_value)
